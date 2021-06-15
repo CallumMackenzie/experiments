ea12c5a346b89f42d0457ab678c4deaeafe527dc
Callum Mackenzie <alexxander1611@gmail.com>
2021-06-14 18:56:56 -0600

    Fix gitignore & remove useless files.

--- astar-cpp/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/callum/astar-cpp
-# Build directory: /home/callum/astar-cpp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
