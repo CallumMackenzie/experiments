c8ae18355702f9fab072f8f5c418043454c008e7
Callum Mackenzie <alexxander1611@gmail.com>
2021-06-22 12:34:30 -0600

    Adding matrix class.

--- math-cpp/math.h
+++ math-cpp/math.h
@@ -3,9 +3,13 @@
 
 #include <initializer_list>
 #include <stddef.h>
+#include <cmath>
 #include <iostream>
 
-typedef long double vec_hash;
+namespace cmm
+{
+
+	typedef long double vec_hash;
 
 #define BIT_OP(OP, TYPE)                                      \
 	friend TYPE operator OP(const TYPE &lhs, const TYPE &rhs) \
@@ -14,7 +18,30 @@ typedef long double vec_hash;
 		for (size_t i = 0; i < TYPE::length; i++)             \
 			ret.vec[i] = lhs.vec[i] OP rhs.vec[i];            \
 		return ret;                                           \
-	}
+	};                                                        \
+	friend TYPE operator OP(const TYPE &lhs, const T &rhs)    \
+	{                                                         \
+		TYPE ret;                                             \
+		for (size_t i = 0; i < TYPE::length; i++)             \
+			ret.vec[i] = lhs.vec[i] OP rhs;                   \
+		return ret;                                           \
+	};                                                        \
+	friend TYPE operator OP(const T &lhs, const TYPE &rhs)    \
+	{                                                         \
+		return rhs * lhs;                                     \
+	};                                                        \
+	TYPE &operator OP##=(const TYPE &rhs)                     \
+	{                                                         \
+		for (size_t i = 0; i < TYPE::length; i++)             \
+			this->vec[i] OP## = rhs.vec[i];                   \
+		return *this;                                         \
+	};                                                        \
+	TYPE &operator OP##=(const T &rhs)                        \
+	{                                                         \
+		for (size_t i = 0; i < TYPE::length; i++)             \
+			this->vec[i] OP## = rhs;                          \
+		return *this;                                         \
+	};
 
 #define UNARY_OP(OP, TYPE)                        \
 	TYPE operator OP()                            \
@@ -34,16 +61,10 @@ typedef long double vec_hash;
 		return cmp_res;                                              \
 	}
 
-#define OSTREAM_OP(TYPE)                                               \
-	friend std::ostream &operator<<(std::ostream &os, const TYPE &val) \
-	{                                                                  \
-		return os << val.to_string();                                  \
-	}
-
-#define ISTREAM_OP(TYPE)                                  \
-	std::istream &operator>>(std::istream &is, TYPE &val) \
-	{                                                     \
-		return val.from_string(is.str());                 \
+#define OSTREAM_OP(...)                                                       \
+	friend std::ostream &operator<<(std::ostream &os, const __VA_ARGS__ &val) \
+	{                                                                         \
+		return os << val.to_string();                                         \
 	}
 
 #define ALGEBRAIC(TYPE) \
@@ -66,45 +87,89 @@ typedef long double vec_hash;
 	PARTIAL_EQ(TYPE);      \
 	OSTREAM_OP(TYPE);
 
-template <typename T, size_t LEN>
-class nvec
-{
-public:
-	static const size_t length = LEN;
-	T vec[LEN];
-
-	nvec() {}
-
-	nvec(std::initializer_list<T> list)
+	template <typename T, size_t LEN>
+	class nvec
 	{
-		size_t i = -1;
-		for (auto item : list)
-			vec[++i] = item;
-	}
+	public:
+		static const size_t length = LEN;
+		T vec[LEN];
 
-	FULL_NUMERIC(nvec)
+		nvec() {}
 
-	T operator[](size_t index)
-	{
-		return vec[index];
-	}
-
-	std::string to_string() const
-	{
-		std::string ret = "{";
-		for (size_t i = 0; i < length; ++i)
+		nvec(std::initializer_list<T> list)
 		{
-			ret.append(std::to_string(vec[i]));
-			if (i == length - 1)
-				ret.append("}");
-			else
-				ret.append(", ");
+			size_t i = -1;
+			for (auto item : list)
+				vec[++i] = item;
 		}
-		return ret;
-	}
 
-	// inline vec_hash hash(nvec v);
-};
+		const inline size_t size() const
+		{
+			return LEN;
+		}
+
+		FULL_NUMERIC(nvec)
+
+		T operator[](const size_t index)
+		{
+			return vec[index];
+		}
+
+		const T operator[](const size_t index) const
+		{
+			return vec[index];
+		}
+
+		nvec &fill(const T val)
+		{
+			for (size_t i = 0; i < LEN; i++)
+				vec[i] = val;
+			return *this;
+		}
+
+		T dot(const nvec &v2) const
+		{
+			T sum = (T)0;
+			for (size_t i = 0; i < LEN; i++)
+				sum += (vec[i] * v2[i]);
+			return sum;
+		}
+
+		T len() const
+		{
+			return std::sqrt(dot(*this));
+		}
+
+		std::string to_string() const
+		{
+			std::string ret = "{";
+			for (size_t i = 0; i < length; ++i)
+			{
+				ret.append(std::to_string(vec[i]));
+				if (i == length - 1)
+					ret.append("}");
+				else
+					ret.append(", ");
+			}
+			return ret;
+		}
+
+		// inline vec_hash hash(nvec v);
+	};
+
+#define LVEC_OP_EQ(NUM_ELEMS)                          \
+	lvec##NUM_ELEMS &operator=(nvec<T, NUM_ELEMS> vec) \
+	{                                                  \
+		for (size_t i = 0; i < this->size(); i++)      \
+			this->vec[i] = vec[i];                     \
+		return *this;                                  \
+	}                                                  \
+	lvec##NUM_ELEMS &operator=(lvec##NUM_ELEMS vec)    \
+	{                                                  \
+		for (size_t i = 0; i < this->size(); i++)      \
+			this->vec[i] = vec[i];                     \
+		return *this;                                  \
+	}
 
 #define LVEC_DECL(NUM_ELEMS)        \
 	template <typename T, size_t N> \
@@ -112,78 +177,72 @@ public:
 	template <typename T>           \
 	using lv##NUM_ELEMS = lvec##NUM_ELEMS<T, NUM_ELEMS>;
 
-LVEC_DECL(1)
-LVEC_DECL(2)
-LVEC_DECL(3)
-LVEC_DECL(4)
+	LVEC_DECL(1)
+	LVEC_DECL(2)
+	LVEC_DECL(3)
+	LVEC_DECL(4)
 
-template <typename T, size_t NELEMS = 1>
-class lvec1 : public nvec<T, NELEMS>
-{
-private:
-	using parent = nvec<T, NELEMS>;
+	template <typename T, size_t NELEMS = 1>
+	class lvec1 : public nvec<T, NELEMS>
+	{
+	private:
+		using parent = nvec<T, NELEMS>;
 
-public:
-	lvec1() : parent() {}
-	lvec1(std::initializer_list<T> list) : parent(list) {}
-	T &x = this->vec[0];
-	lv2<T> xx() { return lv2<T>{x, x}; }
-	lv3<T> xxx() { return lv3<T>{x, x, x}; }
-	lv4<T> xxxx() { return lv4<T>{x, x, x, x}; }
-};
+	public:
+		lvec1() : parent() {}
+		lvec1(std::initializer_list<T> list) : parent(list) {}
+		T &x = this->vec[0];
+		lv2<T> xx() { return lv2<T>{x, x}; }
+		lv3<T> xxx() { return lv3<T>{x, x, x}; }
+		lv4<T> xxxx() { return lv4<T>{x, x, x, x}; }
 
-template <typename T, size_t NELEMS = 2>
-class lvec2 : public lvec1<T, NELEMS>
-{
-private:
-	using parent = lvec1<T, NELEMS>;
+		LVEC_OP_EQ(1)
+	};
 
-public:
-	lvec2() : parent() {}
-	lvec2(std::initializer_list<T> list) : parent(list) {}
-	T &y = this->vec[1];
+	template <typename T, size_t NELEMS = 2>
+	class lvec2 : public lvec1<T, NELEMS>
+	{
+	private:
+		using parent = lvec1<T, NELEMS>;
 
-	lv4<T> yyyy() { return lv4<T>{y, y, y, y}; }
-	lv3<T> yyy() { return lv3<T>{y, y, y}; }
-	lv2<T> yy() { return lv2<T>{y, y}; }
-	lv2<T> yx() { return lv2<T>{y, this->x}; }
-	lv2<T> xy() { return lv2<T>{this->x, y}; }
-	lv3<T> xxy() { return lv3<T>{this->x, this->x, y}; }
-	lv3<T> xyy() { return lv3<T>{this->x, y, y}; }
-	lv3<T> yxx() { return lv3<T>{y, this->x, this->x}; }
-	lv3<T> yyx() { return lv3<T>{y, y, this->x}; }
-	lv3<T> xyx() { return lv3<T>{this->x, y, this->x}; }
-};
+	public:
+		lvec2() : parent() {}
+		lvec2(std::initializer_list<T> list) : parent(list) {}
+		T &y = this->vec[1];
 
-template <typename T, size_t NELEMS = 3>
-class lvec3 : public lvec2<T, NELEMS>
-{
-private:
-	using parent = lvec2<T, NELEMS>;
+		lv4<T> yyyy() { return lv4<T>{y, y, y, y}; }
+		lv3<T> yyy() { return lv3<T>{y, y, y}; }
+		lv2<T> yy() { return lv2<T>{y, y}; }
+		lv2<T> yx() { return lv2<T>{y, this->x}; }
+		lv2<T> xy() { return lv2<T>{this->x, y}; }
+		lv3<T> xxy() { return lv3<T>{this->x, this->x, y}; }
+		lv3<T> xyy() { return lv3<T>{this->x, y, y}; }
+		lv3<T> yxx() { return lv3<T>{y, this->x, this->x}; }
+		lv3<T> yyx() { return lv3<T>{y, y, this->x}; }
+		lv3<T> xyx() { return lv3<T>{this->x, y, this->x}; }
 
-public:
-	lvec3() : parent() {}
-	lvec3(std::initializer_list<T> list) : parent(list) {}
-	T &z = this->vec[2];
+		LVEC_OP_EQ(2)
+	};
 
-	lv2<T> zz() { return lv2<T>{z, z}; }
-	lv3<T> zzz() { return lv3<T>{z, z, z}; }
-	lv4<T> zzzz() { return lv4<T>{z, z, z, z}; }
-	lv3<T> xyz() { return lv3<T>{this->x, this->y, z}; }
-	lv3<T> zyx() { return lv3<T>{z, this->y, this->x}; }
-};
+	template <typename T, size_t NELEMS = 3>
+	class lvec3 : public lvec2<T, NELEMS>
+	{
+	private:
+		using parent = lvec2<T, NELEMS>;
 
-template <typename T, size_t NELEMS = 4>
-class lvec4 : public lvec3<T, NELEMS>
-{
-private:
-	using parent = lvec3<T, NELEMS>;
+	public:
+		lvec3() : parent() {}
+		lvec3(std::initializer_list<T> list) : parent(list) {}
+		T &z = this->vec[2];
 
-public:
-	lvec4() : parent() {}
-	lvec4(std::initializer_list<T> list) : parent(list) {}
-	T &w = this->vec[3];
-};
+		lv2<T> zz() { return lv2<T>{z, z}; }
+		lv3<T> zzz() { return lv3<T>{z, z, z}; }
+		lv4<T> zzzz() { return lv4<T>{z, z, z, z}; }
+		lv3<T> xyz() { return lv3<T>{this->x, this->y, z}; }
+		lv3<T> zyx() { return lv3<T>{z, this->y, this->x}; }
+
+		LVEC_OP_EQ(3)
+	};
 
 #define NEW_LVEC(NUM_ELEMS, PREV_NELEMS, LETTER)                         \
 	template <typename T, size_t NELEMS = NUM_ELEMS>                     \
@@ -196,8 +255,105 @@ public:
 		lvec##NUM_ELEMS() : parent() {}                                  \
 		lvec##NUM_ELEMS(std::initializer_list<T> list) : parent(list) {} \
 		T &LETTER = this->vec[PREV_NELEMS];                              \
+		LVEC_OP_EQ(NUM_ELEMS)                                            \
 	};
 
-NEW_LVEC(5, 4, a)
+	NEW_LVEC(4, 3, w)
+
+	template <typename T, size_t W, size_t H>
+	class mat
+	{
+	public:
+		T m[H][W] = {0};
+
+		mat() {}
+		mat(std::initializer_list<std::initializer_list<T>> list) : mat()
+		{
+			size_t r = 0;
+			size_t c = 0;
+			for (auto row : list)
+			{
+				for (auto val : row)
+				{
+					m[r][c] = val;
+					if (++c >= W)
+						break;
+				}
+				c = 0;
+				if (++r >= H)
+					break;
+			}
+		}
+
+		size_t width() const
+		{
+			return W;
+		}
+
+		size_t height() const
+		{
+			return H;
+		}
+
+		std::string to_string() const
+		{
+			std::string str("{");
+			for (size_t i = 0; i < H; i++)
+			{
+				str.append((i == 0) ? "{" : " {");
+				for (size_t j = 0; j < W; j++)
+					str.append(std::to_string(m[i][j])).append(((j + 1) == W) ? "" : ", ");
+				str.append(((i + 1) == H) ? "}" : "}\n");
+			}
+			return str.append("}");
+		}
+
+		T *operator[](const size_t index)
+		{
+			return m[index];
+		}
+
+		const T *operator[](const size_t index) const
+		{
+			return m[index];
+		}
+
+		friend mat operator*(const T &lhs, const mat &rhs)
+		{
+			mat ret = rhs;
+			for (size_t i = 0; i < ret.height(); i++)
+				for (size_t j = 0; j < ret.width(); j++)
+					ret[i][j] *= lhs;
+			return ret;
+		}
+
+		friend mat operator*(const mat &lhs, const T &rhs)
+		{
+			return rhs * lhs;
+		}
+
+		friend mat operator/(const mat &lhs, const T &rhs)
+		{
+			mat ret = lhs;
+			for (size_t i = 0; i < ret.height(); i++)
+				for (size_t j = 0; j < ret.width(); j++)
+					ret[i][j] /= rhs;
+			return ret;
+		}
+
+		friend mat<T, H, H> operator*(const mat<T, W, H> &lhs, const mat<T, H, W> &rhs)
+		{
+			mat<T, H, H> ret;
+			for (size_t r = 0; r < H; r++)
+				for (size_t c = 0; c < H; c++)
+					for (size_t i = 0; i < W; i++)
+						ret[r][c] += lhs[r][i] * rhs[i][c];
+			return ret;
+		}
+
+		OSTREAM_OP(mat)
+	};
+
+}
 
 #endif
--- math-cpp/tests.cpp
+++ math-cpp/tests.cpp
@@ -3,7 +3,10 @@
 #include <string>
 #include <stdexcept>
 
+#include "memory-aid.h"
 #include "math.h"
+
+COUNT_MEMORY
 
 #define ASSERT(...)                                                                                                                                             \
 	if (!(__VA_ARGS__))                                                                                                                                         \
@@ -11,23 +14,44 @@
 	else                                                                                                                                                        \
 		std::cout << "Test passed." << std::endl;
 
+using namespace cmm;
+
 int main(int, char **)
 {
 
-	ASSERT(lvec2<int>{-1, -2} == -lvec2<int>{1, 2});
-	ASSERT(lvec2<int>{2, 3} * lvec2<int>{-3, 4} == lvec2<int>{-6, 12});
-	ASSERT(lvec2<int>{12, 3} + lvec2<int>{2, -1} == lvec2<int>{14, 2});
-	ASSERT(lvec2<int>{9, 2} - lvec2<int>{8, 1} == lvec2<int>{1, 1});
-	ASSERT(lvec2<int>{100, 80} / lvec2<int>{10, 8} == lvec2<int>{10, 10});
-	ASSERT((lvec2<int>{0b111, 0b001} & lvec2<int>{0b101, 0b100}) == lvec2<int>{0b101, 0b000});
-	ASSERT((lvec2<int>{0b110, 0b010} | lvec2<int>{0b101, 0b001}) == lvec2<int>{0b111, 0b011});
-	ASSERT((!lvec2<int>{0b011, 0b111}) == lvec2<int>{0b000, 0b000});
-	ASSERT((lvec2<int>{0b111, 0b101} ^ lvec2<int>{0b010, 0b111}) == lvec2<int>{0b101, 0b010});
+	{
+		ASSERT(lvec2<int>{-1, -2} == -lvec2<int>{1, 2});
+		ASSERT(lvec2<int>{2, 3} * lvec2<int>{-3, 4} == lvec2<int>{-6, 12});
+		ASSERT(lvec2<int>{12, 3} + lvec2<int>{2, -1} == lvec2<int>{14, 2});
+		ASSERT(lvec2<int>{9, 2} - lvec2<int>{8, 1} == lvec2<int>{1, 1});
+		ASSERT(lvec2<int>{100, 80} / lvec2<int>{10, 8} == lvec2<int>{10, 10});
+		ASSERT((lvec2<int>{0b111, 0b001} & lvec2<int>{0b101, 0b100}) == lvec2<int>{0b101, 0b000});
+		ASSERT((lvec2<int>{0b110, 0b010} | lvec2<int>{0b101, 0b001}) == lvec2<int>{0b111, 0b011});
+		ASSERT((!lvec2<int>{0b011, 0b111}) == lvec2<int>{0b000, 0b000});
+		ASSERT((lvec2<int>{0b111, 0b101} ^ lvec2<int>{0b010, 0b111}) == lvec2<int>{0b101, 0b010});
 
-	auto v = lvec5<int>{1, 2, 3, 4, 5};
-	std::cout << v << std::endl;
+		auto v = lvec4<double>{1, 2, 3, 4};
+		v /= 2.0;
+		v += 1;
+		v -= 0.1;
+		v += lvec4<double>{10, 0, 2, 3};
+		v *= lvec4<double>{3, -3, 2, 0.3};
+		v = (2 * v);
+		v = (v / 2);
+		v += (v - 12);
+		std::cout << v << std::endl;
 #define P << ", " <<
-	std::cout << v.x P v.y P v.z P v.w P v.a << std::endl
-			  << v.xx() P v.xxx() P v.xxxx() << std::endl
-			  << v.xy() P v.yx() << std::endl;
+		std::cout << v.x P v.y P v.z P v.w << std::endl
+				  << v.xx() P v.xxx() P v.xxxx() << std::endl
+				  << v.xy() P v.yx() << std::endl
+				  << std::endl;
+
+		std::cout << lvec4<int>().fill(2) << std::endl;
+
+		auto mt = mat<int, 3, 2>{{1, 2, 3}, {4, 5, 6}};
+		auto mt2 = mat<int, 2, 3>{{7, 8}, {9, 10}, {11, 12}};
+		auto mt3 = mt * mt2;
+		std::cout << mt3 << std::endl;
+	}
+	PRINT_MEMORY_SUMMARY
 }
