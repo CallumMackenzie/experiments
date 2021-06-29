0ee26274ce113a9b4a501d84b9d142ab3ed00f24
Callum Mackenzie <alexxander1611@gmail.com>
2021-06-29 16:53:14 -0600

    Console 3D rendering.

--- tri-rasterize-cpp/main.cpp
+++ tri-rasterize-cpp/main.cpp
@@ -1,24 +1,71 @@
 #include <iostream>
-#include "math.h"
 #include <cstring>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+#include "math.h"
 
 using namespace cmm;
 
-struct vert
+#define WINDING_CLOCKWISE 122
+#define WINDING_COUNTER_CLOCKWISE 233
+#define WINDING WINDING_COUNTER_CLOCKWISE
+
+struct camera_3d
 {
-    nvec<double, 2> v;
-    double depth = 0; // Farthest
-    char sym = ' ';
+    double fov = 80;
+    double near = 0.1;
+    double far = 100;
+    double aspect = 1;
+    lvec3<double> rotation;
+    lvec3<double> position;
+
+    mat<double, 4, 4> view()
+    {
+        lv4<double> up{0, 1, 0};
+        lv4<double> target{0, 0, 1, 1};
+        lv4<double> cam_rot(MFNS<double, 4, 4>::rotation(rotation) * target);
+        target = cam_rot + position.xyz1();
+        auto ret = MFNS<double, 4, 4>::look_at(position.xyz(), target.xyz(), up.xyz());
+        return ret;
+    }
+
+    mat<double, 4, 4> perspective()
+    {
+        return MFNS<double, 4, 4>::projection(fov, aspect, near, far);
+    }
+
+    nvec<double, 4> look_vector()
+    {
+        return MFNS<double, 4, 4>::rotation(rotation) * nvec<double, 4>{0, 0, 1, 1};
+    }
 };
 
-struct tri
+struct triangle_2d
 {
     struct bounding_box
     {
         double top, left, right, bottom = 0;
     };
+    struct vert
+    {
+        nvec<double, 2> v;
+        double depth = 0;
+        lv2<double> t;
+        char sym = ' ';
+    };
 
     vert v[3];
+
+    triangle_2d() {}
+    triangle_2d(nvec<double, 2> v1, nvec<double, 2> v2, nvec<double, 2> v3)
+    {
+        v[0].v = v1;
+        v[1].v = v2;
+        v[2].v = v3;
+    }
 
     void set_sym(char sym)
     {
@@ -43,7 +90,7 @@ struct tri
 };
 
 template <int W, int H>
-struct map2d
+struct console_render_target
 {
     union
     {
@@ -53,10 +100,9 @@ struct map2d
 
     double depth_buff[H][W] = {0};
 
-    map2d()
+    console_render_target()
     {
-        for (int i = 0; i < (H * W); i++)
-            map_arr[i] = ' ';
+        clear();
     }
 
     inline char &point_at(double x, double y)
@@ -67,7 +113,7 @@ struct map2d
     void clear()
     {
         memset(map, ' ', sizeof(map));
-        memset(depth_buff, 0.0, sizeof(depth_buff));
+        memset(depth_buff, -0.001, sizeof(depth_buff));
     }
 
     void print() const
@@ -80,9 +126,9 @@ struct map2d
         }
     }
 
-    void rasterize(tri &tri_in)
+    void rasterize(triangle_2d &tri_in)
     {
-        tri tri_out;
+        triangle_2d tri_out;
         auto edge_fn = [](const nvec<int, 2> &a, const nvec<int, 2> &b, const nvec<int, 2> &c)
         {
             int val = (c[0] - a[0]) * (b[1] - a[1]) - (c[1] - a[1]) * (b[0] - a[0]);
@@ -91,9 +137,15 @@ struct map2d
         auto in_tri = [&tri_out, &edge_fn](nvec<int, 2> p)
         {
             bool inside = true;
+#if WINDING == WINDING_CLOCKWISE
+            inside &= edge_fn(tri_out.v[0].v, tri_out.v[2].v, p);
             inside &= edge_fn(tri_out.v[1].v, tri_out.v[0].v, p);
             inside &= edge_fn(tri_out.v[2].v, tri_out.v[1].v, p);
-            inside &= edge_fn(tri_out.v[0].v, tri_out.v[2].v, p);
+#else
+            inside &= edge_fn(tri_out.v[0].v, tri_out.v[1].v, p);
+            inside &= edge_fn(tri_out.v[1].v, tri_out.v[2].v, p);
+            inside &= edge_fn(tri_out.v[2].v, tri_out.v[0].v, p);
+#endif
             return inside;
         };
         for (int i = 0; i < 3; ++i)
@@ -101,18 +153,20 @@ struct map2d
             tri_out.v[i].v[0] = (int)(tri_in.v[i].v[0] * W);
             tri_out.v[i].v[1] = (int)(tri_in.v[i].v[1] * H);
         }
-        tri::bounding_box box;
+        triangle_2d::bounding_box box;
         tri_out.get_bounding_box(box);
+        if (box.top < 0)
+            box.top = 0;
+        if (box.left < 0)
+            box.left = 0;
+        if (box.right >= W)
+            box.right = W - 1;
+        if (box.bottom >= H)
+            box.bottom = H - 1;
         for (int y = box.top; y <= box.bottom; ++y)
             for (int x = box.left; x <= box.right; ++x)
             {
                 nvec<int, 2> p{x, y};
-                if (y < 0)
-                    y = 0;
-                if (x < 0)
-                    x = 0;
-                if (y >= H || x >= W)
-                    break;
                 if (in_tri(p))
                 {
                     nvec<double, 2> fragCoord{(double)p[0] / (double)W,
@@ -120,72 +174,220 @@ struct map2d
                     double d0 = (fragCoord - tri_in.v[0].v).len();
                     double d1 = (fragCoord - tri_in.v[1].v).len();
                     double d2 = (fragCoord - tri_in.v[2].v).len();
-                    double coord_depth = (d0 * tri_in.v[0].depth) +
-                                         (d1 * tri_in.v[1].depth) +
-                                         (d2 * tri_in.v[2].depth);
-                    if (coord_depth >= depth_buff[y][x])
+                    double frag_depth = (d0 * tri_in.v[0].depth) +
+                                        (d1 * tri_in.v[1].depth) +
+                                        (d2 * tri_in.v[2].depth);
+                    if (frag_depth >= depth_buff[y][x])
                     {
                         map[y][x] = tri_in.v[0].sym;
-                        depth_buff[y][x] = coord_depth;
+                        depth_buff[y][x] = frag_depth;
                     }
                 }
             }
     }
 };
 
+struct mesh_3d
+{
+    struct triangle
+    {
+        struct comp
+        {
+            lvec3<double> p;
+            lvec2<double> t;
+        };
+
+        comp v[3];
+        char sym = '?';
+    };
+
+    std::vector<triangle> tris;
+    lvec3<double> scale{1, 1, 1};
+    lvec3<double> position;
+    lvec3<double> rotation;
+
+    template <size_t W, size_t H>
+    void render(console_render_target<W, H> &target, camera_3d &camera)
+    {
+        size_t n_tris = tris.size();
+        auto projection_m = camera.perspective();
+        auto translation_m = MFNS<double, 4, 4>::translation(position);
+        auto rotation_m = MFNS<double, 4, 4>::rotation(rotation);
+        auto scale_m = MFNS<double, 4, 4>::scale(scale);
+        auto transform = scale_m * rotation_m * translation_m;
+        auto cam_view = camera.view().inverse();
+        for (size_t i = 0; i < n_tris; ++i)
+        {
+            triangle_2d rast_tri;
+            mesh_3d::triangle &in_tri = tris[i];
+            bool draw = true;
+            for (size_t j = 0; j < 3; ++j)
+            {
+                lv4<double> v_pos(projection_m * (cam_view * (transform * in_tri.v[j].p.xyz1())));
+                if (v_pos.w() == 0)
+                {
+                    draw = false;
+                    continue;
+                }
+                v_pos /= v_pos.w();
+                rast_tri.v[j].v = v_pos.xy().to_nvec();
+                rast_tri.v[j].depth = v_pos.z();
+                rast_tri.v[j].sym = in_tri.sym;
+            }
+            if (!draw)
+                continue;
+            target.rasterize(rast_tri);
+        }
+    }
+
+    bool load_from_obj(std::string file_name)
+    {
+        using namespace std;
+        ifstream f(file_name);
+        if (!f.is_open())
+            return false;
+
+        vector<lvec3<double>> verts;
+        vector<lvec2<double>> texs;
+        bool has_tex = false;
+        bool has_norm = false;
+
+        while (!f.eof())
+        {
+            char line[255];
+            char junk;
+            f.getline(line, 255);
+            stringstream s;
+
+            s << line;
+            if (line[0] == 'v')
+            {
+                if (line[1] == 't')
+                {
+                    lvec2<double> v;
+                    s >> junk >> junk >> v[0] >> v[1];
+                    texs.push_back(v);
+                }
+                else if (line[1] == 'n')
+                {
+                    has_norm = true;
+                }
+                else
+                {
+                    lvec3<double> v;
+                    s >> junk >> v[0] >> v[1] >> v[2];
+                    verts.push_back(v);
+                    has_tex = true;
+                }
+            }
+            if (line[0] == 'f')
+            {
+                int face[3] = {0};
+                int text[3] = {0};
+                int ijunk = 0;
+                if (!has_tex && !has_norm)
+                    s >> junk >> face[0] >> face[1] >> face[2];
+                else if (!has_norm)
+                    s >> junk >>
+                        face[0] >> junk >> text[0] >>
+                        face[1] >> junk >> text[1] >>
+                        face[2] >> junk >> text[2];
+                else if (has_norm && has_tex)
+                    s >> junk >>
+                        face[0] >> junk >> text[0] >> junk >> ijunk >>
+                        face[1] >> junk >> text[1] >> junk >> ijunk >>
+                        face[2] >> junk >> text[2] >> junk >> ijunk;
+                triangle fTri;
+                for (int j = 0; j < 3; j++)
+                    fTri.v[j].p = verts[face[j] - 1];
+                if (has_tex)
+                    for (int j = 0; j < 3; j++)
+                        fTri.v[j].t = texs[text[j] - 1];
+                tris.push_back(fTri);
+            }
+        }
+        return true;
+    }
+};
+
+#define MAP_SZ <140, 70>
+
 int main(int, char **)
 {
     std::cout << "Program begin." << std::endl;
-    map2d<80, 40> map;
+    console_render_target MAP_SZ map;
+    camera_3d camera;
+    camera.position = {-2, -1.5, -5};
 
-    tri tri1;
-    tri1.v[0].v = nvec<double, 2>{0.2, 0.2};
-    tri1.v[1].v = nvec<double, 2>{0.8, 0.8};
-    tri1.v[2].v = nvec<double, 2>{0.0, 0.8};
-    tri1.set_sym('*');
-    tri1.v[0].depth = 1;
-    tri1.v[1].depth = 0;
-    tri1.v[2].depth = 0.5;
-
-    tri tri2;
-    tri2.v[0].v = nvec<double, 2>{0.2, 0.2};
-    tri2.v[1].v = nvec<double, 2>{0.8, 0.0};
-    tri2.v[2].v = nvec<double, 2>{0.8, 0.8};
-    tri2.set_sym('#');
-    tri2.v[0].depth = 0.6;
-    tri2.v[1].depth = 0.1;
-    tri2.v[2].depth = 0.3;
-
-    double rot_val = 0;
-    auto rot_pt = nvec<double, 2>{0.5, 0.5};
-    auto scale1 = nvec<double, 2>{1, 1};
-    auto scale_add = nvec<double, 2>{0.001, 0.001};
-    bool scale_flip_flop = true;
+    mesh_3d mesh;
+    mesh.position = {0, 0, 0};
+    // mesh.scale = {1.2, 1.2, 1.2};
+    if (!mesh.load_from_obj("../cube.obj"))
+    {
+        std::cerr << "Could not load OBJ." << std::endl;
+        return 1;
+    }
+    int ctr = 0;
+    const int max_char = 122;
+    const int min_char = 33;
+    for (int i = 0; i < mesh.tris.size(); i++)
+    {
+        char symbol = (min_char + i) - (ctr * (max_char - min_char));
+        mesh.tris[i].sym = symbol;
+        if (symbol == max_char)
+            ++ctr;
+    }
 
     printf("\x1b[2J");
     for (;;)
     {
-        tri temp;
         printf("\x1b[H");
-        if (scale_flip_flop)
-            scale1 += scale_add;
-        else
-            scale1 -= scale_add;
-        if (scale1[0] > 2)
-            scale_flip_flop = false;
-        if (scale1[0] < 0.3)
-            scale_flip_flop = true;
-        temp = tri1;
-        for (int i = 0; i < 3; ++i)
-            temp.v[i].v = (scale1 * (tri1.v[i].v - rot_pt)) + rot_pt;
-        map.rasterize(temp);
-        temp = tri2;
-        rot_val += 0.006;
-        for (int i = 0; i < 3; ++i)
-            temp.v[i].v = (mat<double, 2, 2>::rotation_2d(rot_val) * (tri2.v[i].v - rot_pt)) + rot_pt;
-        map.rasterize(temp);
+        mesh.rotation += {0.007, 0.02, 0.01};
+        // lvec3<double> forward;
+        // lvec3<double> up{0, 1, 0};
+        // lvec3<double> rotate;
+        // nvec<double, 4> clv_nv = camera.look_vector();
+        // lvec3<double> clv{clv_nv[0], clv_nv[1], clv_nv[2]};
+        // switch (to_move)
+        // {
+        // case 'w':
+        //     forward += clv;
+        //     break;
+        // case 's':
+        //     forward -= clv;
+        //     break;
+        // case 'a':
+        //     forward += clv.cross(up);
+        //     break;
+        // case 'd':
+        //     forward -= clv.cross(up);
+        //     break;
+        // case 'q':
+        //     forward += {0, 1, 0};
+        //     break;
+        // case 'e':
+        //     forward -= {0, 1, 0};
+        //     break;
+        // case 'l':
+        //     rotate += {0, 1, 0};
+        //     break;
+        // case 'j':
+        //     rotate -= {0, 1, 0};
+        //     break;
+        // case 'i':
+        //     rotate += {1, 0, 0};
+        //     break;
+        // case 'k':
+        //     rotate -= {1, 0, 0};
+        //     break;
+        // default:
+        //     break;
+        // }
+        // camera.position += forward.normalize() * amount;
+        // camera.rotation += rotate.normalize() * amount;
+
+        mesh.render MAP_SZ(map, camera);
         map.print();
         map.clear();
-        // std::cout << scale_flip_flop << "  " << scale1 << std::endl;
     }
 }
--- tri-rasterize-cpp/math.h
+++ tri-rasterize-cpp/math.h
@@ -87,6 +87,17 @@ namespace cmm
 	PARTIAL_EQ(TYPE);      \
 	OSTREAM_OP(TYPE);
 
+#define LVEC_DECL(NUM_ELEMS)        \
+	template <typename T, size_t N> \
+	class lvec##NUM_ELEMS;          \
+	template <typename T>           \
+	using lv##NUM_ELEMS = lvec##NUM_ELEMS<T, NUM_ELEMS>;
+
+	LVEC_DECL(1)
+	LVEC_DECL(2)
+	LVEC_DECL(3)
+	LVEC_DECL(4)
+
 	template <typename T, size_t LEN>
 	class nvec
 	{
@@ -203,29 +214,18 @@ namespace cmm
 	};
 
 #define LVEC_OP_EQ(NUM_ELEMS)                          \
-	lvec##NUM_ELEMS &operator=(nvec<T, NUM_ELEMS> vec) \
-	{                                                  \
-		for (size_t i = 0; i < this->size(); i++)      \
-			this->vec[i] = vec[i];                     \
-		return *this;                                  \
-	}                                                  \
-	lvec##NUM_ELEMS &operator=(lvec##NUM_ELEMS vec)    \
-	{                                                  \
-		for (size_t i = 0; i < this->size(); i++)      \
-			this->vec[i] = vec[i];                     \
-		return *this;                                  \
-	}
-
-#define LVEC_DECL(NUM_ELEMS)        \
-	template <typename T, size_t N> \
-	class lvec##NUM_ELEMS;          \
-	template <typename T>           \
-	using lv##NUM_ELEMS = lvec##NUM_ELEMS<T, NUM_ELEMS>;
-
-	LVEC_DECL(1)
-	LVEC_DECL(2)
-	LVEC_DECL(3)
-	LVEC_DECL(4)
+	// lvec##NUM_ELEMS &operator=(nvec<T, NUM_ELEMS> vec) \
+	// {                                                  \
+	// 	for (size_t i = 0; i < NUM_ELEMS; i++)         \
+	// 		this->vec[i] = vec[i];                     \
+	// 	return *this;                                  \
+	// }                                                  \
+	// lvec##NUM_ELEMS &operator=(lvec##NUM_ELEMS vec)    \
+	// {                                                  \
+	// 	for (size_t i = 0; i < NUM_ELEMS; i++)         \
+	// 		this->vec[i] = vec[i];                     \
+	// 	return *this;                                  \
+	// }
 
 	template <typename T, size_t NELEMS = 1>
 	class lvec1 : public nvec<T, NELEMS>
@@ -236,10 +236,10 @@ namespace cmm
 	public:
 		lvec1() : parent() {}
 		lvec1(std::initializer_list<T> list) : parent(list) {}
-		T &x = this->vec[0];
-		lv2<T> xx() { return lv2<T>{x, x}; }
-		lv3<T> xxx() { return lv3<T>{x, x, x}; }
-		lv4<T> xxxx() { return lv4<T>{x, x, x, x}; }
+		T &x() { return this->vec[0]; }
+		lv2<T> xx() { return lv2<T>{x(), x()}; }
+		lv3<T> xxx() { return lv3<T>{x(), x(), x()}; }
+		lv4<T> xxxx() { return lv4<T>{x(), x(), x(), x()}; }
 
 		LVEC_OP_EQ(1)
 	};
@@ -253,18 +253,30 @@ namespace cmm
 	public:
 		lvec2() : parent() {}
 		lvec2(std::initializer_list<T> list) : parent(list) {}
-		T &y = this->vec[1];
+		lvec2(nvec<T, 2> base)
+		{
+			for (size_t i = 0; i < 2; i++)
+				this->vec[i] = base.vec[i];
+		}
 
-		lv4<T> yyyy() { return lv4<T>{y, y, y, y}; }
-		lv3<T> yyy() { return lv3<T>{y, y, y}; }
-		lv2<T> yy() { return lv2<T>{y, y}; }
-		lv2<T> yx() { return lv2<T>{y, this->x}; }
-		lv2<T> xy() { return lv2<T>{this->x, y}; }
-		lv3<T> xxy() { return lv3<T>{this->x, this->x, y}; }
-		lv3<T> xyy() { return lv3<T>{this->x, y, y}; }
-		lv3<T> yxx() { return lv3<T>{y, this->x, this->x}; }
-		lv3<T> yyx() { return lv3<T>{y, y, this->x}; }
-		lv3<T> xyx() { return lv3<T>{this->x, y, this->x}; }
+		T &x() { return this->vec[0]; }
+		T &y() { return this->vec[1]; }
+
+		lv4<T> yyyy() { return lv4<T>{y(), y(), y(), y()}; }
+		lv3<T> yyy() { return lv3<T>{y(), y(), y()}; }
+		lv2<T> yy() { return lv2<T>{y(), y()}; }
+		lv2<T> yx() { return lv2<T>{y(), x()}; }
+		lv2<T> xy() { return lv2<T>{x(), y()}; }
+		lv3<T> xxy() { return lv3<T>{x(), x(), y()}; }
+		lv3<T> xyy() { return lv3<T>{x(), y(), y()}; }
+		lv3<T> yxx() { return lv3<T>{y(), x(), x()}; }
+		lv3<T> yyx() { return lv3<T>{y(), y(), x()}; }
+		lv3<T> xyx() { return lv3<T>{x(), y(), x()}; }
+
+		nvec<T, NELEMS> to_nvec()
+		{
+			return nvec<T, NELEMS>{x(), y()};
+		}
 
 		LVEC_OP_EQ(2)
 	};
@@ -278,32 +290,84 @@ namespace cmm
 	public:
 		lvec3() : parent() {}
 		lvec3(std::initializer_list<T> list) : parent(list) {}
-		T &z = this->vec[2];
+		lvec3(parent base, T next)
+		{
+			for (size_t i = 0; i < 2; i++)
+				this->vec[i] = base.vec[i];
+			this->vec[2] = next;
+		}
+		lvec3(nvec<T, 3> base)
+		{
+			for (size_t i = 0; i < NELEMS; i++)
+				this->vec[i] = base.vec[i];
+		}
 
-		lv2<T> zz() { return lv2<T>{z, z}; }
-		lv3<T> zzz() { return lv3<T>{z, z, z}; }
-		lv4<T> zzzz() { return lv4<T>{z, z, z, z}; }
-		lv3<T> xyz() { return lv3<T>{this->x, this->y, z}; }
-		lv3<T> zyx() { return lv3<T>{z, this->y, this->x}; }
+		T &x() { return this->vec[0]; }
+		T &y() { return this->vec[1]; }
+		T &z() { return this->vec[2]; }
+
+		lv2<T> zz() { return lv2<T>{z(), z()}; }
+		lv3<T> zzz() { return lv3<T>{z(), z(), z()}; }
+		lv4<T> zzzz() { return lv4<T>{z(), z(), z(), z()}; }
+		lv3<T> xyz() { return lv3<T>{x(), y(), z()}; }
+		lv3<T> zyx() { return lv3<T>{z(), y(), x()}; }
+		lv4<T> xyz1() { return lv4<T>{x(), y(), z(), (T)1}; }
+
+		nvec<T, NELEMS> to_nvec()
+		{
+			return nvec<T, NELEMS>{x(), y(), z()};
+		}
 
 		LVEC_OP_EQ(3)
 	};
 
-#define NEW_LVEC(NUM_ELEMS, PREV_NELEMS, LETTER)                         \
-	template <typename T, size_t NELEMS = NUM_ELEMS>                     \
-	class lvec##NUM_ELEMS : public lvec##PREV_NELEMS<T, NELEMS>          \
-	{                                                                    \
-	private:                                                             \
-		using parent = lvec##PREV_NELEMS<T, NELEMS>;                     \
-                                                                         \
-	public:                                                              \
-		lvec##NUM_ELEMS() : parent() {}                                  \
-		lvec##NUM_ELEMS(std::initializer_list<T> list) : parent(list) {} \
-		T &LETTER = this->vec[PREV_NELEMS];                              \
-		LVEC_OP_EQ(NUM_ELEMS)                                            \
+	template <typename T, size_t NELEMS = 4>
+	class lvec4 : public lvec3<T, NELEMS>
+	{
+	private:
+		using parent = lvec3<T, NELEMS>;
+
+	public:
+		lvec4() : parent() {}
+		lvec4(std::initializer_list<T> list) : parent(list) {}
+		lvec4(parent base, T next)
+		{
+			for (size_t i = 0; i < 3; i++)
+				this->vec[i] = base.vec[i];
+			this->vec[3] = next;
+		}
+		lvec4(nvec<T, 4> base)
+		{
+			for (size_t i = 0; i < NELEMS; i++)
+				this->vec[i] = base.vec[i];
+		}
+
+		T &x() { return this->vec[0]; }
+		T &y() { return this->vec[1]; }
+		T &z() { return this->vec[2]; }
+		T &w() { return this->vec[3]; }
+
+		nvec<T, NELEMS> to_nvec()
+		{
+			return nvec<T, NELEMS>{x(), y(), z(), w()};
+		}
+
+		LVEC_OP_EQ(4)
 	};
 
-	NEW_LVEC(4, 3, w)
+	struct matrix_exception
+	{
+		std::string what;
+
+		matrix_exception(std::string what_)
+		{
+			what = what_;
+		}
+	};
+
+#define MFNS mat_fns
+	template <typename T, size_t W, size_t H>
+	class MFNS;
 
 	template <typename T, size_t W, size_t H>
 	class mat
@@ -330,12 +394,12 @@ namespace cmm
 			}
 		}
 
-		size_t width() const
+		inline size_t width() const
 		{
 			return W;
 		}
 
-		size_t height() const
+		inline size_t height() const
 		{
 			return H;
 		}
@@ -353,12 +417,74 @@ namespace cmm
 			return str.append("}");
 		}
 
-		T *operator[](const size_t index)
+		mat get_cofactor(int p, int q, int n) const
+		{
+			mat temp;
+			int i = 0, j = 0;
+			for (int row = 0; row < n; ++row)
+				for (int col = 0; col < n; ++col)
+					if (row != p && col != q)
+					{
+						temp[i][j] = m[row][col];
+						if ((++j) == n - 1)
+						{
+							j = 0;
+							++i;
+						}
+					}
+			return temp;
+		}
+
+		T determinant(int n = H) const
+		{
+			if (n == 1)
+				return m[0][0];
+			T D = 0;
+			mat temp;
+			int sign = 1;
+			for (int f = 0; f < n; ++f)
+			{
+				temp = get_cofactor(0, f, n);
+				D += ((T)sign) * m[0][f] * temp.determinant(n - 1);
+				sign = -sign;
+			}
+			return D;
+		}
+
+		mat adjoint() const
+		{
+			if (H == 1)
+				return identity();
+
+			int sign = 1;
+			mat temp;
+			mat adj;
+
+			for (int i = 0; i < H; i++)
+				for (int j = 0; j < W; j++)
+				{
+					temp = get_cofactor(i, j, H);
+					sign = ((i + j) % 2 == 0) ? 1 : -1;
+					adj[j][i] = (T)sign * temp.determinant(H - 1);
+				}
+			return adj;
+		}
+
+		mat inverse() const
+		{
+			T det = determinant();
+			if (det == 0)
+				throw matrix_exception("Cannot find matrix of singular matrix.");
+
+			return adjoint() / det;
+		}
+
+		inline T *operator[](const size_t index)
 		{
 			return m[index];
 		}
 
-		const T *operator[](const size_t index) const
+		inline const T *operator[](const size_t index) const
 		{
 			return m[index];
 		}
@@ -372,7 +498,7 @@ namespace cmm
 			return ret;
 		}
 
-		friend mat operator*(const mat &lhs, const T &rhs)
+		inline friend mat operator*(const mat &lhs, const T &rhs)
 		{
 			return rhs * lhs;
 		}
@@ -396,23 +522,12 @@ namespace cmm
 			return ret;
 		}
 
-		friend nvec<T, H> operator*(const mat<T, W, H> &lhs, const nvec<T, W> &rhs)
+		friend nvec<T, H> operator*(const mat<T, W, H> &lhs, const nvec<T, H> &rhs)
 		{
-			nvec<T, H> ret;
+			nvec<T, W> ret;
 			for (size_t r = 0; r < H; r++)
 				for (size_t c = 0; c < W; c++)
-					ret.vec[r] += rhs[c] * lhs[r][c];
-			return ret;
-		}
-
-		static mat<T, 4, 4> projection_3d(T fovDeg, T aspectRatio, T near, T far)
-		{
-			T fovRad = (T)1 / tanf((fovDeg * (T)((T)377 / (T)120) / (T)180));
-			T zero = (T)0;
-			mat<T, 4, 4> ret{{fovRad * aspectRatio, zero, zero, zero},
-							 {zero, fovRad, zero, zero},
-							 {zero, zero, far / (far - near), (T)1},
-							 {zero, zero, (-far * near) / (far - near), zero}};
+					ret.vec[c] += rhs[r] * lhs[r][c];
 			return ret;
 		}
 
@@ -424,7 +539,29 @@ namespace cmm
 			return ret;
 		}
 
-		static mat<T, 4, 4> lookAt_3d(nvec<T, 3> pos, nvec<T, 3> target, nvec<T, 3> up = nvec<T, 3>{0, 1, 0})
+		OSTREAM_OP(mat)
+	};
+
+	template <typename T>
+	class MFNS<T, 4, 4>
+	{
+	public:
+		static mat<T, 4, 4> projection(T fovDeg, T aspectRatio, T near, T far)
+		{
+			auto deg_to_rad = [](T deg)
+			{
+				return deg * (3.14159265 / 180.000);
+			};
+			T fovRad = 1.00 / tanf(deg_to_rad(fovDeg * 0.500));
+			T zero = (T)0;
+			mat<T, 4, 4> ret{{fovRad * aspectRatio, zero, zero, zero},
+							 {zero, fovRad, zero, zero},
+							 {zero, zero, far / (far - near), (T)1},
+							 {zero, zero, (-far * near) / (far - near), zero}};
+			return ret;
+		}
+
+		static mat<T, 4, 4> look_at(nvec<T, 3> pos, nvec<T, 3> target, nvec<T, 3> up = nvec<T, 3>{0, 1, 0})
 		{
 			auto new_forward = (target - pos).normalize();
 			auto new_up = (up - (new_forward * up.dot(new_forward))).normalize();
@@ -437,7 +574,7 @@ namespace cmm
 			return m;
 		}
 
-		static mat<T, 4, 4> scale_3d(T x, T y, T z)
+		static mat<T, 4, 4> scale(T x, T y, T z)
 		{
 			auto m = mat<T, 4, 4>::identity();
 			m[0][0] = x;
@@ -446,26 +583,29 @@ namespace cmm
 			return m;
 		}
 
-		static inline mat<T, 4, 4> scale_3d(const nvec<T, 3> v)
+		static inline mat<T, 4, 4> scale(const nvec<T, 3> v)
 		{
-			return scale_3d(v[0], v[1], v[2]);
+			return scale(v[0], v[1], v[2]);
 		}
 
-		static mat<T, 4, 4> translation_3d(T x, T y, T z)
+		static mat<T, 4, 4> translation(T x, T y, T z)
 		{
 			auto m = mat<T, 4, 4>::identity();
-			m[0][3] = x;
-			m[1][3] = y;
-			m[2][3] = z;
+			m[3][0] = x;
+			m[3][1] = y;
+			m[3][2] = z;
+			// m[0][3] = x;
+			// m[1][3] = y;
+			// m[2][3] = z;
 			return m;
 		}
 
-		static inline mat<T, 4, 4> translation_3d(const nvec<T, 3> v)
+		static inline mat<T, 4, 4> translation(const nvec<T, 3> v)
 		{
 			return translation(v[0], v[1], v[2]);
 		}
 
-		static mat<T, 4, 4> x_rotation_3d(T x_rad)
+		static mat<T, 4, 4> x_rotation(T x_rad)
 		{
 			auto m = mat<T, 4, 4>::identity();
 			m[1][1] = cosf(x_rad);
@@ -475,7 +615,7 @@ namespace cmm
 			return m;
 		}
 
-		static mat<T, 4, 4> y_rotation_3d(T y_rad)
+		static mat<T, 4, 4> y_rotation(T y_rad)
 		{
 			auto m = mat<T, 4, 4>::identity();
 			m[0][0] = cosf(y_rad);
@@ -485,7 +625,7 @@ namespace cmm
 			return m;
 		}
 
-		static mat<T, 4, 4> z_rotation_3d(T z_rad)
+		static mat<T, 4, 4> z_rotation(T z_rad)
 		{
 			auto m = mat<T, 4, 4>::identity();
 			m[0][0] = cosf(z_rad);
@@ -495,36 +635,39 @@ namespace cmm
 			return m;
 		}
 
-		static inline mat<T, 4, 4> rotation_3d(T x, T y, T z)
+		static inline mat<T, 4, 4> rotation(T x, T y, T z)
 		{
-			return x_rotation_3d(x) * y_rotation_3d(y) * z_rotation_3d(z);
+			return x_rotation(x) * y_rotation(y) * z_rotation(z);
 		}
 
-		static inline mat<T, 4, 4> rotation_3d(nvec<T, 3> v)
+		static inline mat<T, 4, 4> rotation(nvec<T, 3> v)
 		{
-			return rotation_3d(v[0], v[1], v[2]);
+			return rotation(v[0], v[1], v[2]);
 		}
+	};
 
-		static mat<T, 2, 2> rotation_2d(T radians)
+	template <typename T>
+	class MFNS<T, 2, 2>
+	{
+	public:
+		static mat<T, 2, 2> rotation(T radians)
 		{
 			return mat<T, 2, 2>{
 				{cosf(radians), sinf(radians)},
 				{-sinf(radians), cosf(radians)}};
 		}
 
-		static mat<T, 2, 2> scale_2d(T x, T y)
+		static mat<T, 2, 2> scale(T x, T y)
 		{
 			return mat<T, 2, 2>{
 				{x, 0},
 				{0, y}};
 		}
-
-		OSTREAM_OP(mat)
 	};
 
 	template <typename T>
 	using mat4 = mat<T, 4, 4>;
-
+	using m4f = MFNS<double, 4, 4>;
 }
 
 #endif
